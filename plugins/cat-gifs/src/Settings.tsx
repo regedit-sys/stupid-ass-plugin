fa8f15afab016e7281d5cddec73c316fe128d0f6
Lucas <1ne.x.b.0ero@gmail.com>
2025-03-16 15:16:43 +0000

    ?+I'm praying this works

--- plugins/cat-gifs/src/Settings.tsx
+++ plugins/cat-gifs/src/Settings.tsx
@@ -2,7 +2,11 @@ import { Forms } from "@vendetta/ui/components";
 const { FormText } = Forms;
 
 export default () => (
-    <FormText>
-        Hello, world!
-    </FormText>
+    <>
+        {/* <FormText>
+            i have literally nothing to put here
+        </FormText> */}
+        <h1>nuh uh</h1>
+        <img src="https://media1.tenor.com/m/V5r7WRoq42kAAAAC/utrenkl-leonardo-dicaprio.gif"/>
+    </>
 )
