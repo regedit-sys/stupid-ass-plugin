9c61e11b012dcc0a415004119519b32de5d50526
Lucas <1ne.x.b.0ero@gmail.com>
2025-03-16 16:08:58 +0000

    !+I got it to work. Praying it deploys

--- plugins/cat-gifs/src/Settings.tsx
+++ plugins/cat-gifs/src/Settings.tsx
@@ -1,12 +1,17 @@
 import { Forms } from "@vendetta/ui/components";
-const { FormText } = Forms;
+const { FormText,Form } = Forms;
+import { React, ReactNative as RN, stylesheet } from '@vendetta/metro/common'
+
 
 export default () => (
     <>
         {/* <FormText>
             i have literally nothing to put here
         </FormText> */}
-        <h1>nuh uh</h1>
-        <img src="https://media1.tenor.com/m/V5r7WRoq42kAAAAC/utrenkl-leonardo-dicaprio.gif"/>
+        <FormText>WIP, i dunno what im really doing atp buuuuut hi</FormText>
+        <RN.Image 
+            source={{ uri: "https://c.tenor.com/V5r7WRoq42kAAAAC/tenor.gif" }} 
+            style={{ width: 498, height: 245 }} 
+        />
     </>
-)
+);
